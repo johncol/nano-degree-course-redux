5303c924d8c6f364eb2eff3add635464e4b03ba7
John <john.19col@gmail.com>
2018-12-30 18:09:09 -0500

    Add goal reducer, the application reducer, and dispatches several actions to see how state is predictably updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function createStore(reducer) {
+const createStore = reducer => {
   let state;
   const listeners = [];
 
@@ -25,7 +25,7 @@ function createStore(reducer) {
     subscribe,
     dispatch
   };
-}
+};
 
 const TodoAction = {
   ADD_TODO: 'ADD_TODO',
@@ -33,33 +33,128 @@ const TodoAction = {
   TOGGLE_TODO: 'TOGGLE_TODO'
 };
 
-function todoReducer(state = { todos: [] }, action) {
+const todoReducer = (state = [], action) => {
   switch (action.type) {
     case TodoAction.ADD_TODO:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo)
-      };
+      return state.concat(action.todo);
 
     case TodoAction.REMOVE_TODO:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.todoId)
-      };
+      return state.filter(todo => todo.id !== action.todoId);
 
     case TodoAction.TOGGLE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map(todo =>
-          todo.id !== action.todoId
-            ? todo
-            : { ...todo, completed: !todo.completed }
-        )
-      };
+      return state.map(todo =>
+        todo.id !== action.todoId ? todo : { ...todo, done: !todo.done }
+      );
 
     default:
       return state;
   }
-}
+};
 
-const store = createStore(todoReducer);
+const GoalAction = {
+  ADD_GOAL: 'ADD_GOAL',
+  REMOVE_GOAL: 'REMOVE_GOAL',
+  TOGGLE_GOAL: 'TOGGLE_GOAL'
+};
+
+const goalReducer = (state = [], action) => {
+  switch (action.type) {
+    case GoalAction.ADD_GOAL:
+      return state.concat(action.goal);
+
+    case GoalAction.REMOVE_GOAL:
+      return state.filter(goal => goal.id !== action.goalId);
+
+    case GoalAction.TOGGLE_GOAL:
+      return state.map(goal =>
+        goal.id !== action.goalId ? goal : { ...goal, achieved: !goal.achieved }
+      );
+
+    default:
+      return state;
+  }
+};
+
+const appReducer = (state = {}, action) => {
+  return {
+    todos: todoReducer(state.todos, action),
+    goals: goalReducer(state.goals, action)
+  };
+};
+
+const store = createStore(appReducer);
+store.subscribe(() => console.log('state: ', store.getState()));
+
+store.dispatch({
+  type: TodoAction.ADD_TODO,
+  todo: {
+    id: 1,
+    name: 'Check tattoos',
+    done: false
+  }
+});
+
+store.dispatch({
+  type: TodoAction.ADD_TODO,
+  todo: {
+    id: 2,
+    name: 'Complete first nano-degree project',
+    done: true
+  }
+});
+
+store.dispatch({
+  type: TodoAction.ADD_TODO,
+  todo: {
+    id: 3,
+    name: 'Complete first lesson in chaptor 4 of nano-degree',
+    done: false
+  }
+});
+
+store.dispatch({
+  type: TodoAction.TOGGLE_TODO,
+  todoId: 3
+});
+
+store.dispatch({
+  type: TodoAction.REMOVE_TODO,
+  todoId: 2
+});
+
+store.dispatch({
+  type: GoalAction.ADD_GOAL,
+  goal: {
+    id: 1,
+    name: 'Finish nano-degree',
+    achieved: false
+  }
+});
+
+store.dispatch({
+  type: GoalAction.ADD_GOAL,
+  goal: {
+    id: 2,
+    name: 'Learn to drive',
+    achieved: false
+  }
+});
+
+store.dispatch({
+  type: GoalAction.ADD_GOAL,
+  goal: {
+    id: 3,
+    name: 'Run half-marathon',
+    achieved: true
+  }
+});
+
+store.dispatch({
+  type: GoalAction.TOGGLE_GOAL,
+  goalId: 2
+});
+
+store.dispatch({
+  type: GoalAction.REMOVE_GOAL,
+  goalId: 2
+});
