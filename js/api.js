eff24cfc098d384b137b476c0952a9efd1a627a4
John <john.19col@gmail.com>
2019-01-09 14:11:37 -0500

    Updates

---
+++ js/api.js
@@ -0,0 +1,136 @@
+(function() {
+  window.API = {};
+
+  function fail() {
+    return Math.floor(Math.random() * (5 - 1)) === 3;
+  }
+
+  function generateId() {
+    return Math.random()
+      .toString(36)
+      .substring(2);
+  }
+
+  var goals = [
+    {
+      id: generateId(),
+      name: 'Learn Redux',
+      achieved: false
+    },
+    {
+      id: generateId(),
+      name: 'Read 50 books this year',
+      achieved: false
+    }
+  ];
+
+  var todos = [
+    {
+      id: generateId(),
+      name: 'Walk the dog',
+      done: false
+    },
+    {
+      id: generateId(),
+      name: 'Wash the car',
+      done: false
+    },
+    {
+      id: generateId(),
+      name: 'Go to the gym',
+      done: true
+    }
+  ];
+
+  window.API.fetchGoals = function() {
+    return new Promise((res, rej) => {
+      setTimeout(function() {
+        res(goals);
+      }, 2000);
+    });
+  };
+
+  window.API.fetchTodos = function() {
+    return new Promise((res, rej) => {
+      setTimeout(function() {
+        res(todos);
+      }, 2000);
+    });
+  };
+
+  window.API.saveTodo = function(name) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const todo = {
+          id: generateId(),
+          name: name,
+          done: false
+        };
+        todos = todos.concat([todo]);
+        fail() ? rej(todo) : res(todo);
+      }, 300);
+    });
+  };
+
+  window.API.saveGoal = function(name) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const goal = {
+          id: generateId(),
+          name: name,
+          achieved: false
+        };
+        goals = goals.concat([goal]);
+        fail() ? rej(goal) : res(goal);
+      }, 300);
+    });
+  };
+
+  window.API.deleteGoal = function(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        goals = goals.filter(goal => goal.id !== id);
+        fail() ? rej() : res(goals);
+      }, 300);
+    });
+  };
+
+  window.API.deleteTodo = function(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.filter(todo => todo.id !== id);
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  };
+
+  window.API.saveTodoToggle = function(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.map(todo =>
+          todo.id !== id
+            ? todo
+            : Object.assign({}, todo, {
+                done: !todo.done
+              })
+        );
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  };
+
+  window.API.saveGoalToggle = function(id) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        goals = goals.map(goal =>
+          goal.id !== id
+            ? goal
+            : Object.assign({}, goal, {
+                achieved: !goal.achieved
+              })
+        );
+        fail() ? rej() : res(goals);
+      }, 300);
+    });
+  };
+})();
---
+++ precache-manifest.55c9006fbcac7288c6c032295fe12006.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ead069aa85831b616733",
+    "url": "/nano-degree-redux/static/js/runtime~main.ead069aa.js"
+  },
+  {
+    "revision": "e126aac88a7e6ce10e67",
+    "url": "/nano-degree-redux/static/js/main.e126aac8.chunk.js"
+  },
+  {
+    "revision": "1ea156595ea1b324c395",
+    "url": "/nano-degree-redux/static/js/1.1ea15659.chunk.js"
+  },
+  {
+    "revision": "3d3b9a1bcb0118f614527b0a6f198bb6",
+    "url": "/nano-degree-redux/index.html"
+  }
+];
